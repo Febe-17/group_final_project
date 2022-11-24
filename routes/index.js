e00349a7df53cf80d20712998845ca40dceecf9d
riskidwiputra <riskidwiputra1250@gmail.com>
2022-11-24 09:48:50 +0700

    add stukture folder

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express           = require('express')
+const router            = express.Router();
+
+// const LoginRouter        = require('./api/login.js')
+
+// api
+
+// cth route login
+// router.use("/api/auth",         LoginRouter);
+
+
+module.exports = router;
