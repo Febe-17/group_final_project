8366d1d69d2af5f636d6f3251849965e0853bed7
riskidwiputra <riskidwiputra1250@gmail.com>
2022-11-25 17:12:07 +0700

    add model kategori and seeeder

---
+++ seeders/20221125084320-create-kategori.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     await queryInterface.bulkInsert(
+      'kategori',
+       [
+        {
+          nama: 'Seni Rupa',
+          gambar : 'https://i.ibb.co/HB6F66x/Rectangle-1274.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          nama: 'Seni Tari',
+          gambar : 'https://i.ibb.co/8shkccZ/Rectangle-1273.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          nama: 'Seni Sastra',
+          gambar : 'https://i.ibb.co/DLkpRGp/Rectangle-1274.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          nama: 'Seni Musik',
+          gambar : 'https://i.ibb.co/1qg8Bpm/Rectangle-1274.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          nama: 'Seni Pertujukan',
+          gambar : 'https://i.ibb.co/rfhLtpY/Rectangle-1274.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+
+      ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     await queryInterface.bulkDelete('kategori', null, {});
+  }
+};
