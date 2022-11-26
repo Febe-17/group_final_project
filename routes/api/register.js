dc4e24be303129184f5a86da1de26a3c3b9f08c1
riskafaradilla <114325558+riskafaradilla@users.noreply.github.com>
2022-11-26 20:09:20 +0800

    Add files via upload
    Signed-off-by: riskafaradilla <114325558+riskafaradilla@users.noreply.github.com>

--- routes/api/register.js
+++ routes/api/register.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const registerValidate  = require("../../validate/validateRegist")
 const router = express.Router();
 
 const {register}  = require("../../controllers/registerController")
-router.post("/register", register);
+router.post("/register", registerValidate, register);
 
 module.exports = router;
