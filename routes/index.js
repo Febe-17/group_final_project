b36ce6ac71eb890ae25282bcd2b34fc8c411c95d
riskafaradilla <114325558+riskafaradilla@users.noreply.github.com>
2022-11-26 13:09:17 +0800

    Update index.js
    Signed-off-by: riskafaradilla <114325558+riskafaradilla@users.noreply.github.com>

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ const express           = require('express')
 const router            = express.Router();
 
 // const LoginRouter        = require('./api/login.js')
+const registerRouter = require('./api/register.js')
 
 // api
 
 // cth route login
 // router.use("/api/auth",         LoginRouter);
+router.use("/api/auth", registerRouter);
 
 
 module.exports = router;
