f28546198f81441357788fd240ad6ff545f21996
riskidwiputra <riskidwiputra1250@gmail.com>
2022-11-29 19:12:34 +0700

    add

--- routes/api/course.js
+++ routes/api/course.js
@@ -1,14 +1,14 @@
 const express                       = require('express')
 const verifyToken                   = require("../../middleware/verifyToken");
 const router                        = express.Router();
-const {create,findCourse}           = require("../../controllers/CourseController.js")
+const {create,findCourse,deleteByid}           = require("../../controllers/CourseController.js")
 
 // router.use(verifyToken);
 
 router.post("/course",              create);
 router.get("/course/:url",          findCourse)
 // router.put("/kategori/:id",        checkKategori ,updateById);
-// router.delete("/kategori/:id",     deleteByid);
+router.delete("/course/:id",     deleteByid);
 // router.get("/all-kategori/:url",    findKategori);
 module.exports = router;
 
